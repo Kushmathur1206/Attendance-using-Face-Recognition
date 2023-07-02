a05bf6c9d3be5133a0471686e7ecd5bea9b28b6a
Kushmathur1206 <Kushmathur1206@gmail.com>
2023-07-02 09:16:03 +0530

    initial commit

---
+++ Main.py
@@ -0,0 +1,75 @@
+import cv2
+import numpy as np
+import csv
+import os 
+from datetime import datetime
+import face_recognition
+
+
+fp=r'photos'
+lf = os.listdir(fp)
+
+known_face_encoding = []
+known_face_names = []
+
+video_capture = cv2.VideoCapture(0)
+
+for yfn in lf:
+    known_face_names.append(yfn.split('.')[0])
+
+
+for i in range(len(lf)):
+    Bill_image = face_recognition.load_image_file("photos/"+lf[i])
+    Bill_encoding  = face_recognition.face_encodings(Bill_image)[0]
+    known_face_encoding.append(Bill_encoding)
+
+for i in range(len(lf)):
+    #print(known_face_encoding[i])
+    print(known_face_names[i])
+
+students = known_face_names.copy()
+
+face_locations = []
+face_encodings = []
+face_names = []
+s=True
+
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+
+f = open(current_date + '.csv','w+',newline = '')
+inwriter = csv.writer(f)
+
+while True:
+    _,frame = video_capture.read()
+    small_frame = cv2.resize(frame,(0,0),fx=0.25, fy=0.25)
+    rgb_small_frame = small_frame[:,:,::-1]
+    if s:
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
+        face_names = []
+        for face_encoding in face_encodings:
+            matches = face_recognition.compare_faces(known_face_encoding,face_encoding)
+            name = ""
+            face_distance = face_recognition.face_distance(known_face_encoding,face_encoding)
+            best_match_index = np.argmin(face_distance)
+            if matches[best_match_index]:
+                name = known_face_names[best_match_index]
+            
+            face_names.append(name)
+            if name in known_face_names:
+                if name in students:
+                    students.remove(name)
+                    print(students)
+                    current_time = now.strftime("%H-%M-%S")
+                    inwriter.writerow([name,current_time])
+        
+        cv2.imshow("Attendance System", frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+        
+
+video_capture.release()
+cv2.destroyAllWindows()
+
+f.close()
