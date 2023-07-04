d74f7c7ef181fd6c456c8301cf2065829cc2c5b5
Kush Mathur <99969817+Kushmathur1206@users.noreply.github.com>
2023-07-04 17:20:57 +0530

    Main.py

--- Main.py
+++ Main.py
@@ -43,7 +43,8 @@ inwriter = csv.writer(f)
 while True:
     _,frame = video_capture.read()
     small_frame = cv2.resize(frame,(0,0),fx=0.25, fy=0.25)
-    rgb_small_frame = small_frame[:,:,::-1]
+    #rgb_small_frame = small_frame[:,:,::-1]
+    rgb_small_frame = np.ascontiguousarray(small_frame[:, :, ::-1])
     if s:
         face_locations = face_recognition.face_locations(rgb_small_frame)
         face_encodings = face_recognition.face_encodings(rgb_small_frame,face_locations)
